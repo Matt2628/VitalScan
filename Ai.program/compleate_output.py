342be633db2a30b0e02767f07e1b6510088c635d
Rosmari Fernandes Flatebø <276310@stud.inn.no>
2026-02-17 18:55:16 +0100

    Update compleate_output.py

--- Ai.program/compleate_output.py
+++ Ai.program/compleate_output.py
@@ -1,30 +1,29 @@
 
-
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import preprocessing_input
+from tensorflow.keras.applications.resnet50 import preprocess_input
 
 # Output fil
-from output import translate_prediction 
+from translated_data import translate_prediction 
 
 # Laster ned ferdig lagd modell
 model = tf.keras.models.load_model("alzheimer_model.h5")
 
 # Sett in test bilde
-image_path = r #"Sett in hva du vil"
+image_path = r"C:\Users\rosma\Documents\Maskinlæring II - Programmering\Python - Vår Semester 2026\Gruppe_Oppgave_1\Prosjekt\Testbilder\41598_2023_41576_Fig1_HTML.jpg"
 
 # Behandle bilde
-img = image.load_img(image_path, taget_size=(224, 224))
+img = image.load_img(image_path, target_size=(224, 224))
 img_array = image.img_to_array(img)
 img_array = np.expand_dims(img_array, axis=0)
-img_array = preprocessing_input(img_array)
+img_array = preprocess_input(img_array)
 
 # Kjør modellen
 pred = model.predict(img_array)
 
 # Oversettelse
-main_burden, class_probs, burden_probs = translate_prediction(pred)
+main_burden, burden_probs = translate_prediction(pred)
 
 # Gjør at hvert av stadiene kommer i ønsket rekkefølge
 order = ["No evidence of disease (NED)",
@@ -37,22 +36,28 @@ patient_name = input("Enter full name of patient: ")
 examination_date = input("Enter examination date (YYYY-MM-DD): ")
 
 print("|------------------------------------------------------------|")
+print("|                                                            |")
 print("|              MRI BRAIN - DIAGNOSTIC SUMMARY                |")
+print("|                                                            |")
 print("|------------------------------------------------------------|")
-print(f"| Patient Name: {patient_name}".ljust(60) + "|")                              
-print(f"| Examination Date: {examination_date}".ljust(60) + "|")
+print("|                                                            |")
+print("| PATIENT AND EXAMINATION DETAILS                            |")
+print("|                                                            |")
+print(f"| Patient Name: {patient_name}".ljust(61) + "|")                              
+print(f"| Examination Date: {examination_date}".ljust(61) + "|")
 print("| Diagnostic Software: VITALScan                             |")                                 
 print("| Imaging Modality: MRI Brain                                |")
 print("|------------------------------------------------------------|")
+print("|                                                            |")
 print("| DIAGNOSTIC BURDEN DISTRIBUTION                             |")
 print("|                                                            |")
-print("| Tumor Burden Assesment:")
+print("| Tumor Burden Assesment:                                    |")
 
 for label in order: 
     line = f"| • {label}: {burden_probs[label]:.1f}%"
-    print(line.ljust(60) + "|")
-
+    print(line.ljust(61) + "|")
+print("|------------------------------------------------------------|")
 print("|                                                            |")
-print("| Overall clinical impression:                               |")
-print(f"| {main_burden}".ljust(60) + "|")
+print("| OVERALL CLINICAL IMPRESSION:                               |")
+print(f"| {main_burden}".ljust(61) + "|")
 print("|------------------------------------------------------------|")
